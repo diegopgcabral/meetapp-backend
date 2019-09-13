754d86e66251d11a4df312afff2b62a5bf794931
diegopgcabral <diegopgc@gmail.com>
2019-09-13 10:33:51 -0300

    Feature: Uploads de arquivos

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: 'c28ac6762525be6f6aceabd5a8ece5ab',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionControler from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 // Cadastro de usuário
 routes.post('/users', UserController.store);
@@ -18,5 +22,8 @@ routes.use(authMiddleware);
 
 // Alteração dados do usuário
 routes.put('/users', UserController.update);
+
+// Upload do arquivo
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
